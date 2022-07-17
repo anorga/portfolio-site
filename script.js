7ecf269b0360b66c18d69f8333add930cdb416bd
Christian Anorga <anorga2990@gmail.com>
2022-07-17 11:46:39 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+/*jslint es6 */
+/*global window */
+/*global document */
+
+const clicker = document.getElementById('clicker');
+clicker.addEventListener('click', () => {
+    'use strict';
+    window.alert('You clicked me!');
+});
+
+
+
